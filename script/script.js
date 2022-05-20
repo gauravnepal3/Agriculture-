ae645b44eb536284d58dee7a88767f9dcc77f904
Gaurav Nepal <gauravnepal3@gmail.com>
2022-05-20 17:26:04 +0545

    layout of about page

--- script/script.js
+++ script/script.js
@@ -1,3 +1,40 @@
 $('#banner-close-btn').click(function() {
     $('#banner').hide(0);
 });
+
+$('call-container').hide(0);
+
+$('#map-btn').click(function() {
+    $('#map-container').show(0);
+    $('#call-container').hide(0);
+});
+
+$('#call-btn').click(function() {
+    $('#call-container').show(0);
+    $('#map-container').hide(0);
+});
+
+// SLICK SLIDER
+
+$(document).ready(function() {
+    $('.customer-logos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
