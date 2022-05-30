aa28a3545d92f30208de07c1241b4521f79c2278
Gaurav Nepal <gauravnepal3@gmail.com>
2022-05-30 15:35:02 +0545

    final changes

--- script/script.js
+++ script/script.js
@@ -38,3 +38,20 @@ $(document).ready(function() {
         }]
     });
 });
+
+window.onscroll = () => {
+    toggleTopButton();
+}
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function toggleTopButton() {
+    if (document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20) {
+        document.getElementById('back-to-up').classList.remove('d-none');
+    } else {
+        document.getElementById('back-to-up').classList.add('d-none');
+    }
+}
