812188bc5568169a46317d367e46408849715119
Gaurav Nepal <gauravnepal3@gmail.com>
2022-06-01 15:25:34 +0545

    updated Contact form

--- script/script.js
+++ script/script.js
@@ -39,19 +39,23 @@ $(document).ready(function() {
     });
 });
 
-window.onscroll = () => {
-    toggleTopButton();
-}
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
 
-function toggleTopButton() {
-    if (document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20) {
-        document.getElementById('back-to-up').classList.remove('d-none');
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $('#myBtn').show(0);
     } else {
-        document.getElementById('back-to-up').classList.add('d-none');
+        $('#myBtn').hide(0);
+
     }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
